3b83c42dc499dbb4ca7c7ab11b3482a260750ae0
sa23225154dzq <2225133982@qq.com>
2024-11-23 14:28:47 +0800

    LRUCache

--- java_demo/src/lru/LRUCache.java
+++ java_demo/src/lru/LRUCache.java
@@ -1,6 +1,7 @@
 package lru;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -9,5 +10,80 @@ import org.junit.jupiter.api.Test;
  * @Description
  */
 public class LRUCache {
+    private int size, capacity; //当前大小、最大容量
+    private Node head, tail; //头尾节点
+    private Map<Integer, Node> dataMap;
+    LRUCache(){}
+    LRUCache(int capacity) {
+        dataMap = new HashMap<Integer, Node>();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+        this.capacity = capacity;
+    }
 
+    //Node内部类
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(){}
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public void put(int key, int value) {
+        Node newNode;
+        if (dataMap.containsKey(key)) {
+            newNode = dataMap.get(key);
+            newNode.value = value;
+            moveNodeToHead(newNode);
+        } else {
+            newNode = new Node(key, value);
+            if (size >= capacity) {
+                Node toDelete = tail.prev;
+                deleteNode(toDelete);
+                dataMap.remove(toDelete.key);
+            }
+            insertNodeIntoHead(newNode);
+        }
+        size++;
+        dataMap.put(key, newNode);
+    }
+
+    public int get(int key) {
+        if (dataMap.containsKey(key)) {
+            Node res = dataMap.get(key);
+            moveNodeToHead(res);
+            return res.value;
+        }
+        return -1;
+    }
+
+    public int top() {
+        if (size == 0) {
+            return -1;
+        }
+        return dataMap.get(head.next.key).value;
+    }
+
+    public void moveNodeToHead(Node node) {
+        deleteNode(node);
+        insertNodeIntoHead(node);
+    }
+
+    public void deleteNode(Node toDelete) {
+        toDelete.prev.next = toDelete.next;
+        toDelete.next.prev = toDelete.prev;
+        size--;
+    }
+
+    public void insertNodeIntoHead(Node toInsert) {
+        toInsert.next = head.next;
+        head.next.prev = toInsert;
+        head.next = toInsert;
+        toInsert.prev = head;
+    }
 }
--- java_demo/src/lru/LRUCacheTest.java
+++ java_demo/src/lru/LRUCacheTest.java
@@ -2,6 +2,8 @@ package lru;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author dzq
  * @Date 2024/11/23 10:05
@@ -9,7 +11,48 @@ import org.junit.Test;
  */
 public class LRUCacheTest {
     @Test
-    public void testLRUCache() {
+    public void testPutAndGet() {
+        LRUCache cache = new LRUCache(3);
 
+        cache.put(1, 10);
+        cache.put(2, 20);
+        cache.put(3, 30);
+
+        assertEquals(10, cache.get(1));
+        assertEquals(20, cache.get(2));
+        assertEquals(30, cache.get(3));
+    }
+
+    @Test
+    public void testCapacityExceeded() {
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 10);
+        cache.put(2, 20);
+        cache.put(3, 30);
+
+        assertEquals(-1, cache.get(1));
+        assertEquals(20, cache.get(2));
+        assertEquals(30, cache.get(3));
+    }
+
+    @Test
+    public void testMoveNodeToHead() {
+        LRUCache cache = new LRUCache(3);
+
+        cache.put(1, 10);
+        cache.put(2, 20);
+        cache.put(3, 30);
+
+        cache.get(2);
+
+        assertEquals(20, cache.top());
+    }
+
+    @Test
+    public void testEmptyCache() {
+        LRUCache cache = new LRUCache(3);
+
+        assertEquals(-1, cache.top());
     }
 }
