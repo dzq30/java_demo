00b69a6cf3acc92cb05ec5e05d9eb31903a54096
sa23225154dzq <2225133982@qq.com>
2024-11-23 10:21:03 +0800

    initail commit

---
+++ java_demo/src/lru/LRUCache.java
@@ -0,0 +1,13 @@
+package lru;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author dzq
+ * @Date 2024/11/23 10:04
+ * @Description
+ */
+public class LRUCache {
+
+}
---
+++ java_demo/src/lru/LRUCacheTest.java
@@ -0,0 +1,15 @@
+package lru;
+
+import org.junit.Test;
+
+/**
+ * @author dzq
+ * @Date 2024/11/23 10:05
+ * @Description
+ */
+public class LRUCacheTest {
+    @Test
+    public void testLRUCache() {
+
+    }
+}
