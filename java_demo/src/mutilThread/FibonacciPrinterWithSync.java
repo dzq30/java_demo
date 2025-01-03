fa192c2fcbe79aa0c4110d4f1073cc3e6ea76b74
sa23225154dzq <2225133982@qq.com>
2025-01-03 17:05:46 +0800

    Revert "多线程"
    This reverts commit 75698dd4d7509b70052f19c5922a6852b15d3eb9.

--- java_demo/src/mutilThread/FibonacciPrinterWithSync.java
+++ java_demo/src/mutilThread/FibonacciPrinterWithSync.java
@@ -5,7 +5,7 @@ package mutilThread;
  * @Date 2025/1/3 14:39
  * @Description
  */
-public class FibonacciPrinterWithSync{
+public class FibonacciPrinterWithSync extends Thread {
     int currentThreadId; // 当前应该打印的线程id
     int fib, current, next; // fib相关参数
     int n, count; // 要打印的总数、已打印的个数
@@ -25,7 +25,6 @@ public class FibonacciPrinterWithSync{
                     throw new RuntimeException(e);
                 }
             }
-            if (count >= n) return;
             fib = current;
             current = next;
             next = fib + current;
@@ -35,24 +34,34 @@ public class FibonacciPrinterWithSync{
             notifyAll();
         }
     }
-}
 
-class SyncMain {
-    public static void main(String[] args) {
-        //test multiThread();
-        FibonacciPrinterWithSync fibonacciPrinter = new FibonacciPrinterWithSync(15);
-        Thread thread1 = new Thread(() -> {
-            fibonacciPrinter.printFibonacci(0);
-        });
-        Thread thread2 = new Thread(() -> {
-            fibonacciPrinter.printFibonacci(1);
-        });
-        Thread thread3 = new Thread(() -> {
-            fibonacciPrinter.printFibonacci(2);
-        });
-        thread1.start();
-        thread2.start();
-        thread3.start();
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    /**
+     * @author dzq
+     * @Date 2025/1/3 15:28
+     * @Description
+     */
+    static class SyncMain {
+        public static void main(String[] args) {
+            //test multiThread();
+            FibonacciPrinterWithSync fibonacciPrinter = new FibonacciPrinterWithSync(15);
+            Thread thread1 = new Thread(() -> {
+                fibonacciPrinter.printFibonacci(0);
+            });
+            Thread thread2 = new Thread(() -> {
+                fibonacciPrinter.printFibonacci(1);
+            });
+            Thread thread3 = new Thread(() -> {
+                fibonacciPrinter.printFibonacci(2);
+            });
+            thread1.start();
+            thread2.start();
+            thread3.start();
+        }
     }
 }
 
